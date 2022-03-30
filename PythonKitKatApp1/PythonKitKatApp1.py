46f1a811a6140331238c85cc250dc51e4596b010
Mike Davies <mike-gitlab@mikedavies.net>
2022-03-31 00:20:41 +0100

    Shrunk

--- PythonKitKatApp1/PythonKitKatApp1.py
+++ PythonKitKatApp1/PythonKitKatApp1.py
@@ -1,25 +1,16 @@
-def kitkat(max):
-    if max != 0:
-        for i in range(1, 100, 1):
-            found_kit = False
-            found_kat = False
-
-            if i % 3 == 0: 
-                found_kit = True;
-            if i % 5 == 0: 
-                found_kat = True;
-
-            if found_kit == True and found_kat == True:
-                print("KitKat")
-            elif found_kit == True:
-                print("Kit")
-            elif found_kat == True:
-                print("Kat")
-            else:
-                print(i)
+def kitkat():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("KitKat")
+        elif i % 3 == 0:
+            print("Kit")
+        elif i % 5 == 0:
+            print("Kat")
+        else:
+            print(i)
 
 def main():
-    kitkat(100)
+    kitkat()
 
 if __name__ == "__main__":
     main()
