55ace9873c5bfeb39dca3c00295d2f7fb14c9741
Mike Davies <mike-gitlab@mikedavies.net>
2022-03-31 00:29:19 +0100

    kitkatnodiv

--- PythonKitKatApp1/PythonKitKatApp1.py
+++ PythonKitKatApp1/PythonKitKatApp1.py
@@ -1,3 +1,23 @@
+def kitkatnodiv():
+    threecount=0
+    fivecount=0
+    for i in range(1, 101):
+        threecount+=1
+        fivecount+=1
+
+        if threecount == 3 and fivecount == 5:
+            threecount=0
+            fivecount=0
+            print("KitKat")
+        elif threecount == 3:
+            threecount=0
+            print("Kit")
+        elif fivecount == 5:
+            fivecount=0
+            print("Kat")
+        else:
+            print(i)
+
 def kitkat():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
@@ -11,6 +31,7 @@ def kitkat():
 
 def main():
     kitkat()
+    kitkatnodiv()
 
 if __name__ == "__main__":
     main()
